f654b3d52d119cd894bfac19089ca45468b08999
Guessan <effiguessan26@gmail.com>
2017-04-11 20:54:48 -0400

    Calculator

---
+++ index.js
@@ -0,0 +1,17 @@
+function addition(){
+	//console.log("Let's add");
+	var math = 7 + 7;
+	console.log(math);
+}
+function subtraction(){
+	//console.log("Let's subtract");
+	console.log(7 - 7);
+}
+function multiplication(){
+	//console.log("Let's multiply");
+	console.log(7 * 7);
+}
+function division(){
+	//console.log("Let's divide");
+	console.log(7 / 7);
+}

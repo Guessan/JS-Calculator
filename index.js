a3481c3668e1ebbc57f5e963d1dae9a3c3aab642
Guessan <effiguessan26@gmail.com>
2017-04-13 20:41:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+var num1;
+var num2;
+var res;
+var results;
+
+document.addEventListener("DOMContentLoaded", function(){
+	num1 = document.getElementById("num1");
+	num1 = document.getElementById("num2");
+	num1 = document.getElementById("results");
+});
+
+
 function addition(){
+	results = +num1.value + +num2.value
+	res.value = results;
 	//console.log("Let's add");
-	var math = 7 + 7;
-	console.log(math);
+	//var math = 7 + 7;
+	//console.log(math);
 }
 function subtraction(){
+	results = +num1.value - +num2.value
+	//res.value = results;
 	//console.log("Let's subtract");
-	console.log(7 - 7);
+	//console.log(7 - 7);
 }
 function multiplication(){
+	results = +num1.value * +num2.value
+	//res.value = results;
 	//console.log("Let's multiply");
 	console.log(7 * 7);
 }
 function division(){
+	results = +num1.value / +num2.value
+	//res.value = results;
 	//console.log("Let's divide");
-	console.log(7 / 7);
+	//console.log(7 / 7);
 }
